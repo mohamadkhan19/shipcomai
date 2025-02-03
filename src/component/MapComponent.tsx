d10b59514586582d1ba264675b6c2ba141b829ae
Mohamad Khan <mohamadkhan19@gmail.com>
2025-02-03 17:28:42 -0600

    Phase0 :: [LEFT SIDE PANEL] | added code structure | material UI | Side bar | Icons | icon highlight functionality

---
+++ src/component/MapComponent.tsx
@@ -0,0 +1,19 @@
+function MapComponent() {
+  return (
+    <div className="App">
+      <h1>Hi</h1>
+      <h1>Hi</h1>
+
+      <h1>Hi</h1>
+      <h1>Hi</h1>
+      <h1>Hi</h1>
+      <h1>Hi</h1>
+      <h1>Hi</h1>
+      <var>
+        <h1>Hi</h1>
+      </var>
+    </div>
+  );
+}
+
+export default MapComponent;
